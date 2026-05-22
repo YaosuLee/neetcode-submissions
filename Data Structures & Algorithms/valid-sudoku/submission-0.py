9a26a9723cdfa3df1bc729841ebdd1bfb494a73a
Jimmy <67504528+YaosuLee@users.noreply.github.com>
2026-05-23 02:07:46 +0800

    Add: valid-sudoku - submission-0

---
+++ Data Structures & Algorithms/valid-sudoku/submission-0.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        seen = set()
+        for i in range(9):
+            for j in range(9):
+                val = board[i][j]
+                if val == ".":
+                    continue
+                
+                row = f"r{i}-{val}"
+                col = f"c{j}-{val}"
+                box = f"b{i//3}-{j//3}-{val}"
+                
+                if (row in seen) or (col in seen) or (box in seen):
+                    return False
+
+                seen.add(row)
+                seen.add(col)
+                seen.add(box)
+        return True
